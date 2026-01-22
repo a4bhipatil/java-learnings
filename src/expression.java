32149908d5701f345ce654f75255f0ffc6465288
abhilash <abhilashpatil921@gmail.com>
2026-01-22 07:52:02 +0530

    learning java

---
+++ src/expression.java
@@ -0,0 +1,17 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import java.util.Scanner;
+
+class expression {
+    public static void main(String[] var0) {
+        Scanner var4 = new Scanner(System.in);
+        System.out.println("Enter the Base and hight");
+        float var1 = var4.nextFloat();
+        float var2 = var4.nextFloat();
+        float var3 = var2 * var1 / 2.0F;
+        System.out.println("Area of a Traingle is " + var3);
+    }
+}

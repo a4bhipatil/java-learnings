bbaf363dcbe29d74aef3dcb2581c5c4d22ed01e6
abhilash <abhilashpatil921@gmail.com>
2026-01-26 12:45:10 +0530

    Storing data of smaller size data type into a large type data can be done automatically

---
+++ src/WideNarrodemo.java
@@ -0,0 +1,20 @@
+public class WideNarrodemo {
+    public static void main(String[] args){
+        byte b=10;
+        short s=10;
+        int i=10;
+        long l=10;
+        float f=10;
+        double d=10;
+        char c=10;
+        boolean b1=true;
+
+        b=(byte)s;//Narrowing of b=s;
+        //s=b;
+        //i=b;
+        //l=b;
+        //f=b;
+        //d=b;
+
+    }
+}

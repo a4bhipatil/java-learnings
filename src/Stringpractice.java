41527c4dca65b1b2a35778bb2e73d05090176a69
abhilash <abhilashpatil921@gmail.com>
2026-01-26 13:37:15 +0530

    about string object

--- src/Stringpractice.java
+++ src/Stringpractice.java
@@ -1,5 +1,16 @@
 public class Stringpractice {
     public static void main(String[] args){
+//    String str1="java programming";
+//    String str2=new String("java");
+//    char c[] ={'H','E','L','L','O'};
+//    byte b[] ={65, 66, 67, 68};
+//    String str3=new String(b,2,2);
+//    System.out.println(str3);
+        //pool
+        String str1="Java";
+        //String str2="Java";
+        String str2=new String("Java");
+        System.out.println(str1==str2);
 
     }
 }

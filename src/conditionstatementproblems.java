eaba6c218d57b5b13515c7907e4f008ade783cea
abhilash <abhilashpatil921@gmail.com>
2026-01-31 12:15:01 +0530

    in this i learn about radix and leap year

--- src/conditionstatementproblems.java
+++ src/conditionstatementproblems.java
@@ -1,20 +1,21 @@
+import java.lang.*;
 import java.util.*;
 public class conditionstatementproblems {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
        //find largest in 3 numbers
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
-        if(a>b&&a>c){
-            System.out.println("largest number is a: "+a);
-        }
-        else if(b>c) {
-                System.out.println("largest number is b: " + b);
-            }
-        else{
-            System.out.println("largest number is c:"+c);
-        }
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        int c=sc.nextInt();
+//        if(a>b&&a>c){
+//            System.out.println("largest number is a: "+a);
+//        }
+//        else if(b>c) {
+//                System.out.println("largest number is b: " + b);
+//            }
+//        else{
+//            System.out.println("largest number is c:"+c);
+//        }
 
         //Check the given number is odd or even
 //        int n=sc.nextInt();
@@ -55,5 +56,42 @@ public class conditionstatementproblems {
 //            System.out.println("The grade is F");
 //        }
 
+        //Find radix of a number given in a string
+//        System.out.println("Enter the number");
+//        String num=sc.nextLine();
+//        if(num.matches("[01]+")){
+//            System.out.println("Binary radix:2");
+//        }
+//        else if(num.matches("[0-7]+")){
+//            System.out.println("octal radix:8");
+//        }
+//        else if(num.matches("[0-9A-F]+")){
+//            System.out.println("Hexadecimal radix:16");
+//        }
+//        else if(num.matches("[0-9]+")){
+//            System.out.println("Decimal radix:10");
+//        }
+//        else {
+//            System.out.println("Invalid number");
+//        }
+
+
+        //Find a given number is leap year
+        System.out.println("Enter the year");
+        int year=sc.nextInt();
+        if(year%4==0){
+            if(year%100==0){
+                if(year%400==0){
+                    System.out.println("It is a leap year");
+                }else {
+                    System.out.println("It is not a leap year");
+                }
+            }else {
+                System.out.println("It is leap Year");
+            }
+        }
+        else {
+            System.out.println("it is not leap year");
+        }
     }
 }

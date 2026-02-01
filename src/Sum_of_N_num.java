d564b4817f894a8b2e7cd2f556152f197da0e3b0
abhilash <abhilashpatil921@gmail.com>
2026-02-01 11:04:55 +0530

    Sum of n Numbers using for loop proble was solved

--- src/Sum_of_N_num.java
+++ src/Sum_of_N_num.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Sum_of_N_num {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        //Find sum of n numbers
+        System.out.println("Enter the numbers");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = sum+i;
+        }
+        System.out.println("Sum of "+n+" Numbers is: "+sum);
+    }
 }

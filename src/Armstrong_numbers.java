764265b5507b8cd442576bc37d7c5376242c3f61
abhilash <abhilashpatil921@gmail.com>
2026-02-02 08:36:56 +0530

    Armstrong number is the a number that equals the sum of its own digits, each raised to the power of the total number of digits is called armstrong using for loop

--- src/Armstrong_numbers.java
+++ src/Armstrong_numbers.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Armstrong_numbers {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number to check the number is Armstrong or not");
+        int n=sc.nextInt();
+        int m=n;
+        int sum=0;
+        int r;
+        while(n>0){
+            r=n%10;
+            n=n/10;
+            sum=sum+r*r*r;
+        }
+        if(sum==m)
+            System.out.println("The number is a Armstrong: ");
+        else
+            System.out.println("The number is not a Armstrong: ");
+
+
+    }
 }

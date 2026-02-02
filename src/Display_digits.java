a6dbf59c8c12c1cec198e30d6f65f7944f07cde8
abhilash <abhilashpatil921@gmail.com>
2026-02-02 08:33:25 +0530

    In this program display the digits by given numbers

--- src/Display_digits.java
+++ src/Display_digits.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Display_digits {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int n=sc.nextInt();
+        while (n>0){
+            int r=n%10;
+            n=n/10;
+            System.out.println(r);
+        }
+    }
 }

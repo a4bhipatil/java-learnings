ab168e908a84e7540db43b7a95b9bf22fca72d73
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:30:18 +0530

    it is just practice purpose

--- src/Practice.java
+++ src/Practice.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Practice {
+   int gcd(int x,int y){
+       while(x!=y){
+           if(x>y)
+               x=x-y;
+           else
+               y=y-x;
+       }
+       return x;
+   }
+   public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       Practice mc=new Practice();
+       System.out.println(mc.gcd(a,b));
+   }
 }

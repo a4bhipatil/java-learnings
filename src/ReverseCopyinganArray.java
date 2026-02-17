d48dfa69e424e07e0983e7404727d8121d024fee
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:32:56 +0530

    using for loop revers an array

--- src/ReverseCopyinganArray.java
+++ src/ReverseCopyinganArray.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class ReverseCopyinganArray {
+    public static void main(String[] args){
+        int A[]={3,9,7,8,12,6,15,5,4,10};
+        int B[]=new int[10];
+        for(int i=A.length-1, j=0; i>=0; i--,j++){
+            B[j]=A[i];
+        }
+        for(int x:B)
+            System.out.print(x+" ");
+        System.out.println("");
+    }
 }

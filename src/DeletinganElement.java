30ea1485fa5139f5dbb24b6756364bde06abe17b
abhilash <abhilashpatil921@gmail.com>
2026-02-17 19:58:55 +0530

    using for loops deleting an array element by index number

--- src/DeletinganElement.java
+++ src/DeletinganElement.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class DeletinganElement {
+    public static void main(String[] args){
+        int A[]={3,9,7,8,12,6,15,5,4,10};
+        for(int i=0; i<A.length; i++)
+            System.out.print(A[i]+" ");
+        System.out.println("");
+        int index=3;
+        for(int i=index; i<A.length-1; i++){
+            A[i]=A[i+1];
+        }
+        for(int i=0; i<A.length-1; i++)
+            System.out.print(A[i]+" ");
+        System.out.println("");
+    }
 }

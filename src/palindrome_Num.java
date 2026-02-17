7dc582b866e6c0b0ac698664cadcfe6d66d5d020
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:25:40 +0530

    What ever we read the word whis same as biggining and ending sequence

--- src/palindrome_Num.java
+++ src/palindrome_Num.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class palindrome_Num {
-}
+        public static void main(String[] args){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the number");
+            int n=sc.nextInt();
+            int m=n;
+            int revers=0;
+            while(n>0){
+                int r=n%10;
+                revers=revers*10+r;
+                n=n/10;
+            }
+            if(revers==m){
+                System.out.print("It is a palindrome: "+revers);
+            }
+            else{
+                System.out.println("It is not a palindrome: "+revers);
+            }
+        }
+    }
+
+

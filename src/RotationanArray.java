79765e4a42d13789aa89f575bb63545ed222dd91
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:33:40 +0530

    rotaion of an array using for looop

--- src/RotationanArray.java
+++ src/RotationanArray.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class RotationanArray {
+    public static void main(String[] args){
+        int A[]={5,9,6,10,12,7,3,5,4,2};
+        for(int x:A)
+            System.out.print(x+",");
+        System.out.println("");
+        int temp=A[0];
+        for(int i=1; i<A.length; i++){
+            A[i-1]=A[i];
+        }
+        A[A.length-1]=temp;
+        for(int x:A)
+            System.out.print(x+",");
+        System.out.println("");
+
+        //another way
+//        int temp=A[0];
+//        for(int i=1; i<A.length; i++){
+//            System.out.print(A[i]+",");
+//        }
+//        A[A.length-1]=temp;
+//        System.out.print(A[A.length-1]);
+    }
 }

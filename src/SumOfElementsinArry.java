664aa52f4be1ca36014b61f89c78ecdae19a48f1
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:36:42 +0530

    sum of elements using for loop in given array

--- src/SumOfElementsinArry.java
+++ src/SumOfElementsinArry.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class SumOfElementsinArry {
+    public static void main(String[] args){
+        int A[]={3,9,7,8,12,6,15,5,4,10};
+        int sum=0;
+        for(int i=0; i<A.length; i++){
+            sum=sum+A[i];
+        }
+        System.out.print(sum);
+
+        for(int x:A){
+            sum=sum+x;
+        }
+        System.out.print(sum);
+    }
 }

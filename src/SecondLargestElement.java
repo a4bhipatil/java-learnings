98f5b06cb6abe0790bd78a0a3c4012aad419e4cc
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:35:20 +0530

    finding an secondlargest element in the array using for loop

--- src/SecondLargestElement.java
+++ src/SecondLargestElement.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class SecondLargestElement {
+    public static void main(String[] args){
+        System.out.println("Find the maximum element in the array");
+        int A[]={3,9,7,8,12,6,15,5,4,10};
+        int max1=A[0];
+        int max2=A[0];
+        for(int i=0; i<A.length; i++){
+            if(max1<A[i]) {
+                max2 = max1;
+                max1=A[i];
+            }
+            else if(max2<A[i])
+                max2=A[i];
+        }
+        System.out.print(max2);
+
+    }
 }

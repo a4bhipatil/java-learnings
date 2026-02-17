4f96ccbab4738c840feb2887b2888b281767a8ab
abhilash <abhilashpatil921@gmail.com>
2026-02-17 19:56:58 +0530

    using for loop copying an array in one array to another array

--- src/CopyinganArray.java
+++ src/CopyinganArray.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class CopyinganArray {
+    public static void main(String[] args){
+        int A[]={3,9,7,8,12,6,15,5,4,10};
+        int B[]=new int[10];
+        for(int i=0; i<A.length; i++)
+            B[i]=A[i];
+        for(int x:B)
+            System.out.print(x+" ");
+    }
 }

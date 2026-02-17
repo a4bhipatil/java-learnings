a079b1c6285f1c578eec7f074a2b3ebed67e4887
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:05:35 +0530

    Increasing size of array is formed by using new array and using for loop then equal the both array in a loop

--- src/IncreasingSizeofArry.java
+++ src/IncreasingSizeofArry.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class IncreasingSizeofArry {
+    public static void main(String[] args){
+        int A[]={3,9,7,8,12};
+        int B[]=new int[2*A.length];
+        for(int i=0; i<A.length; i++)
+            B[i]=A[i];
+        A=B;
+        for(int x:A)
+            System.out.print(x+" ");
+        System.out.println("");
+        System.out.print(A.length);
+    }
 }

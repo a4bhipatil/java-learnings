971632bfed16c8067926180058cb0852bd16c1e5
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:13:34 +0530

    It is the simple problem which we find the maximum element using for loop and if condition

--- src/MaximumElement.java
+++ src/MaximumElement.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class MaximumElement {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Find the maximum element in the array");
+        int A[]={3,9,7,8,12,6,15,5,4,10};
+        int max=A[0];
+        for(int i=0; i<A.length; i++){
+            if(max<A[i])
+                max=A[i];
+        }
+        System.out.print(max);
+    }
 }

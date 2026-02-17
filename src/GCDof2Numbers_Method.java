60cf5bd6ba7c66cabe327c60176f5216d54c3c3c
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:01:20 +0530

    using method find the greatest common divisor logic is m>n(m=m-n) and n>m(n=n-m)

--- src/GCDof2Numbers_Method.java
+++ src/GCDof2Numbers_Method.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class GCDof2Numbers_Method {
+    static int GCD(int m , int n){
+        while (m!=n){
+            if (m < n)
+                n = n - m;
+            else
+                m = m - n;
+        }
+        return m;
+    }
+    public static void main(String[] args){
+        int a=35,b=56;
+        System.out.println(GCD(a,b));
+    }
 }

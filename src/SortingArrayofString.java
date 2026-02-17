69dd267c128d23250ee1aaf62ee7fa671d15d85d
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:36:00 +0530

    Using for loop sorting an string element

--- src/SortingArrayofString.java
+++ src/SortingArrayofString.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class SortingArrayofString {
+    public static void main(String[] args){
+        String arr[] ={"java","python","ada","basic","pascal","smalltalk"};
+        java.util.Arrays.sort(arr);
+        for(String x:arr){
+            System.out.println(x);
+        }
+    }
 }

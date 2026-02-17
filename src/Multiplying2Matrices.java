a85a4a8e2e7946b31ae7e2911e2c16ab1c5d5d34
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:20:37 +0530

    Multiplying two matrix using for loops

--- src/Multiplying2Matrices.java
+++ src/Multiplying2Matrices.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Multiplying2Matrices {
+    public static void main(String[] args){
+        int A[][]=new int[3][3];
+        A= new int[][]{{3,5,9}, {7,6,2}, {4,3,5}};
+        int B[][]=new int[3][3];
+        B= new int[][]{{1,0,0}, {0,1,0}, {0,0,1}};
+        int c[][]=new int[3][3];
+        for(int i=0; i<A.length; i++){
+            for(int j=0; j<A.length; j++){
+                c[i][j]=0;
+                for(int k=0; k<A.length; k++){
+                    c[i][j]+=A[i][k]*B[k][j];
+                }
+            }
+        }
+
+        for(int x[]:c){
+            for(int y:x){
+                System.out.print(y+" ");
+            }
+            System.out.println("");
+        }
+    }
 }

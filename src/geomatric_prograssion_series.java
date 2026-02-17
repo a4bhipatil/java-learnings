90bb9453e4b5907ff60e10c6b12b5f8d6033cbcd
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:03:29 +0530

    geomatric progression is a sequence of where each term after the first is found by multipying the previos term by a fixed non zero number

--- src/geomatric_prograssion_series.java
+++ src/geomatric_prograssion_series.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class geomatric_prograssion_series {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int r = sc.nextInt();
+        int n = sc.nextInt();
+        int term=a;
+        for(int i=0; i<n; i++){
+            System.out.print(term+",");
+            term=term*r;
+        }
+    }
 }

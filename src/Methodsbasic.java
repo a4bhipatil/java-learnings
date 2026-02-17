8269e1910dd552cb9cae2fc077d151259857c823
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:15:55 +0530

    It is all about the basic of method

--- src/Methodsbasic.java
+++ src/Methodsbasic.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Methodsbasic {
+//    int max(int x , int y){
+//        if(x>y)
+//            return x;
+//        else
+//            return y;
+//    }
+//    public static void main(String[] args){
+//        int a=10 , b=15,c;
+//        Methodsbasic ms=new Methodsbasic();
+//        System.out.println(ms.max(a,b));
+//
+//    }
+
+
+    static void inc(int x){
+        x++;
+        System.out.println(x);
+    }
+    public static void main(String[] args){
+        int a=10,b=15;
+        inc(a);
+        System.out.println(a);
+    }
 }

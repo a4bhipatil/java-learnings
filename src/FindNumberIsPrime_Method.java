e2987a817a4d33f5b183f4cb1e9263873113b16e
abhilash <abhilashpatil921@gmail.com>
2026-02-17 19:59:47 +0530

    using method check the number is prime or not

--- src/FindNumberIsPrime_Method.java
+++ src/FindNumberIsPrime_Method.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FindNumberIsPrime_Method {
+    static boolean isprime(int n){
+        for(int i=2; i<n/2; i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(isprime(a));
+    }
 }

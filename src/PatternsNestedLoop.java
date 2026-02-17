a40dedbbb9fe9cb902674798038bc3d65001c060
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:27:58 +0530

    It just using for loop and write a pattern

--- src/PatternsNestedLoop.java
+++ src/PatternsNestedLoop.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class PatternsNestedLoop {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        for(int i=0; i<n; i++){
+//            for(int j=0; j<n; j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println("");
+//        }
+
+//        int n=sc.nextInt();
+//        for(int i=1; i<=n; i++){
+//            for(int j=1; j<=n; j++){
+//                System.out.print(i+j+" ");
+//            }
+//            System.out.println("");
+//        }
+
+        int n=sc.nextInt();
+        int count=0;
+        for(int i=1; i<=n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.format("%02d ",++count);
+            }
+            System.out.println("");
+        }
+    }
 }

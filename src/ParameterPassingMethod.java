8b3f6c0bee41dcccd78869439fb172a0d7b378f9
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:26:41 +0530

    Using method to passing the parameter

--- src/ParameterPassingMethod.java
+++ src/ParameterPassingMethod.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class ParameterPassingMethod {
+
+    // it is type of integer value passing
+//    static int add(int x,int y){
+//        int z;
+//        z=x+y;
+//        return z;
+//    }
+//    public static void main(String[] args){
+//        int a=10,b=15,c;
+//        c=add(a,b);
+//        System.out.println(c);
+//    }
+
+    //now for string passing;
+    static void welcome(String n){
+        System.out.println("welcome mr/ms "+n);
+    }
+    public static void main(String[] args){
+        String name="Abhilash";
+        welcome(name);
+    }
 }

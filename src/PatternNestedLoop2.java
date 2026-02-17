6b7f116fcba09f33ff549cb54efe79bf1dbdf34e
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:27:14 +0530

    It just using for loop and write a patterns

--- src/PatternNestedLoop2.java
+++ src/PatternNestedLoop2.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class PatternNestedLoop2 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+//        for(int i=1; i<n; i++){
+//            for(int j=1; j<n; j++){
+//                if(j>=i){
+//                    System.out.print("* ");
+//                }
+//                else{
+//                    System.out.print("  ");
+//                }
+//            }
+//            System.out.println("");
+//        }
+
+
+//        for(int i=0; i<=n; i++){
+//            for(int j=0; j<=n; j++){
+//                if(i+j>n)
+//                    System.out.print("* ");
+//                else
+//                    System.out.print("  ");
+//            }
+//            System.out.println("");
+//        }
+
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i+j>n)
+                    System.out.print("* ");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println("");
+        }
+        for(int i=1; i<=n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j>i)
+                    System.out.print("* ");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
 }

dd494610bdba04a5b6c198ef0915cb162c586c16
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:23:48 +0530

    using method passing the object from formal method to actual method

--- src/ObjectPassinginMethod.java
+++ src/ObjectPassinginMethod.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class ObjectPassinginMethod {
+    static void update(int X[],int index,int value){
+        X[index]=value;
+    }
+    public static void main(String[] args){
+        int A[]={1,2,3,4,5};
+        update(A,2,25);
+        System.out.println(A[0]);
+        for(int x:A)
+            System.out.print(x+" ");
+    }
 }

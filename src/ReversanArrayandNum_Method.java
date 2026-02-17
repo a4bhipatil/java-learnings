23243118cd736ebc1da854940fe6a69c742794e3
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:31:25 +0530

    Using method revers an array

--- src/ReversanArrayandNum_Method.java
+++ src/ReversanArrayandNum_Method.java
@@ -1,15 +1,34 @@
-public class ReversanArray_Method {
-    static int Revers(int A[]){
-        for(int i=A.length-1; i>=0; i--)
-            for(int j=0; j<B.length; j++)
-                B[j]=A[i];
-        return 0;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReversanArrayandNum_Method {
+    static int Revers(int n){
+        int revers=0;
+        while(n>0){
+            int r=n%10;
+            revers=revers*10+r;
+            n=n/10;
+        }
+        return revers;
     }
-    public static void main(String[] args){
-        int A[]={2,4,64,6,42,8,9,5};
-        int B[];
-        int result=Revers(A);
-        for(int x:B)
-            System.out.println(x);
+    static int[] Revers(int A[]){
+        int B[] =new int[A.length];
+        for(int i = 0; i<A.length; i++){
+            B[i]=A[A.length-1-i];
+        }
+        return B;
+    }
+//    public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int result=Revers(n);
+//        System.out.println(result);
+//    }
+
+     public static void main(String[] args) {
+         int A[]={2,5,6,7,4,8,9,4};
+         int[] B=Revers(A);
+         for (int x:B)
+             System.out.print(x+" ");
     }
 }

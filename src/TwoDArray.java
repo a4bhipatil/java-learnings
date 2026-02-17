f4e611ae64e88f385ae6dc50222a0a837281d8f7
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:37:13 +0530

    all about Two dim array

--- src/TwoDArray.java
+++ src/TwoDArray.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class TwoDArray {
+    public static void main(String[] args){
+        int A[][]=new int[3][4];
+        A= new int[][]{{3, 9, 7, 8}, {12, 6, 15, 5}, {4, 10, 5, 6}};
+//        for(int i=0; i<A.length; i++){
+//            for(int j=0; j<A[0].length; j++){
+//                System.out.format("%02d ",A[i][j]);
+//            }
+//            System.out.println("");
+//        }
+
+        for(int x[]:A){
+            for(int y:x){
+                System.out.format("%02d ",y);
+            }
+            System.out.print("\n");
+        }
+    }
 }

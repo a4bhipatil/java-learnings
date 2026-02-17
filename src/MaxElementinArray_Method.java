2d03907c0c7c55b4dbc12cb475ceca8a2fd45094
abhilash <abhilashpatil921@gmail.com>
2026-02-17 20:12:26 +0530

    Using method finding the max element in given array

--- src/MaxElementinArray_Method.java
+++ src/MaxElementinArray_Method.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class MaxElementinArray_Method {
+    static int Max(int A[]){
+        int max=A[0];
+        for(int i=1; i<A.length; i++){
+            if(A[i]>max)
+                max=A[i];
+        }
+        return max;
+    }
+    public static void main(String[] args){
+        int A[]={3,5,7,2,9,15,1};
+        int max = 0;
+        int result=Max(A);
+        System.out.println(result);
+    }
 }

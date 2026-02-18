1ff40831a31baaf51028859abc5d9c40b3ca29e9
abhilash <abhilashpatil921@gmail.com>
2026-02-18 19:49:39 +0530

    using variable arguments find the sum of all elemets with for loop

--- src/SumofAllElementsUsingVarargs_Method.java
+++ src/SumofAllElementsUsingVarargs_Method.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class SumofAllElementsUsingVarargs_Method {
+    static int Sum(int ...A){
+        if(A.length==0) return Integer.MIN_VALUE;
+        int S=0;
+        for(int i=0; i<A.length; i++) {
+            S = S + A[i];
+        }
+        return S;
+    }
+    public static void main(String[] args){
+        System.out.println(Sum(10,20,30,40,50));
+    }
 }

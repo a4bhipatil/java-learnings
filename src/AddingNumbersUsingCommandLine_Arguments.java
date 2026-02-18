3bcf61865b150c84847c10ec95b06efd5556426a
abhilash <abhilashpatil921@gmail.com>
2026-02-18 19:53:04 +0530

    Using commandline argument to adding numbers

--- src/AddingNumbersUsingCommandLine_Arguments.java
+++ src/AddingNumbersUsingCommandLine_Arguments.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class AddingNumbersUsingCommandLine_Arguments {
+    public static void main(String[] args){
+        double sum=0;
+        for(int i=0; i<args.length; i++){
+//            if(args[i].matches("[0-9\\.]+"))
+                sum=sum+ Double.parseDouble(args[i]);
+        }
+        System.out.println(sum);
+    }
 }

d188d73e095062a6f580d6a5c585c652024ea729
abhilash <abhilashpatil921@gmail.com>
2026-02-18 19:51:41 +0530

    using variable arguments to show the array elements and strings

--- src/VariableArgument_Method.java
+++ src/VariableArgument_Method.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class VariableArgument_Method {
+//    static void show(int ...A){
+//        for(int x:A)
+//            System.out.print(x+" ");
+//    }
+//
+//    public static void main(String[] args){
+//        show();
+//        show(1,3,45,64,4,3);
+//        show(new int[]{10,20,30,40,50});
+//    }
+
+
+    static void showList(String ...S){
+        for(int i=0; i<S.length; i++)
+            System.out.println(i+1+". "+S[i]);
+    }
+    public static void main(String[] args){
+        showList("John","Smith","Ajay","Mark","Suresh");
+    }
 }

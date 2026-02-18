2301ed8f39cc197ac0ee4830f4def2041703d904
abhilash <abhilashpatil921@gmail.com>
2026-02-18 19:55:30 +0530

    using variable arguments in methods and for loop and condition statment to find the discount calculation

--- src/DiscountCalculationUsingVarargs_Method.java
+++ src/DiscountCalculationUsingVarargs_Method.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class DiscountCalculationUsingVarargs_Method {
+    static double Discount(double ...A){
+        int amount=0;
+        for(int i=0; i<A.length; i++){
+            amount +=A[i];
+        }
+        if(amount<500)
+            return amount*0.10;
+        else if(amount>=500 && amount<1000)
+            return amount*0.15;
+        else
+            return amount*0.20;
+    }
+    public static void main(String[] args){
+        System.out.print(Discount()+" ");
+        System.out.print(Discount(10)+" ");
+        System.out.print(Discount(10,20,30)+" ");
+        System.out.print(Discount(10,20,30,40,50)+" ");
+    }
 }

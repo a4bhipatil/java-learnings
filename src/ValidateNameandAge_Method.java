02deeee8516ad1442ccc54cf54ad6c3e18796dc9
abhilash <abhilashpatil921@gmail.com>
2026-02-18 19:50:42 +0530

    It is the problem using methods to validate the name and age

--- src/ValidateNameandAge_Method.java
+++ src/ValidateNameandAge_Method.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ValidateNameandAge_Method {
+    static boolean validate(String name){
+        return name.matches("[a-zA-Z\\s]+");
+    }
+    static boolean validate(int age){
+        return age>=3 && age<=15;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name=sc.next();
+        System.out.println(validate(name));
+        int age=sc.nextInt();
+        System.out.println(validate(age));
+
+    }
 }

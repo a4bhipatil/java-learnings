a50b3c4491933db7ca7a7ae7ad6dacf60ba5280b
abhilash <abhilashpatil921@gmail.com>
2026-02-18 19:57:18 +0530

    Using variable argument to find the maximum number in the array and using for loop and if else condition statement

--- src/MaximumNumberusingVarargu_Method.java
+++ src/MaximumNumberusingVarargu_Method.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class MaximumNumberusingVarargu_Method {
+    static int show(int ...A){
+        if(A.length==0) return Integer.MIN_VALUE;
+        int m=A[0];
+        for(int i=0; i<A.length; i++ )
+            if(A[i]>m)
+                m=A[i];
+        return m;
+    }
+    public static void main(String[] args){
+        System.out.println(show(1,2,3,5,7,43,8,3,63,3));
+    }
 }

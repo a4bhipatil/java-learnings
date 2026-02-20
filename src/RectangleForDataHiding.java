f86b29a467b6ad3f7dac73558a3a241cd7039d65
abhilash <abhilashpatil921@gmail.com>
2026-02-20 07:46:32 +0530

    Data hiding concept is used to solve the rectangle in this problem the length and breadth are private so we use read for get method and write for set method

--- src/RectangleForDataHiding.java
+++ src/RectangleForDataHiding.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+class rectangletest{
+    private double length;
+    private double breadth;
 
+    public double getLength(){
+        return length;
+    }
+    public double getBreadth(){
+        return breadth;
+    }
+    public void setLength(double l){
+        length=l;
+    }
+    public void setBreadth(double b){
+        breadth=b;
+    }
+
+    public double Area(){
+        return length*breadth;
+    }
+    public double parameter(){
+        return 2*length+2*breadth;
+    }
+    public boolean isSquare(){
+        if(length==breadth)
+            return true;
+        else
+            return false;
+    }
+}
 public class RectangleForDataHiding {
+    public static void main(String[] args){
+        rectangletest r1=new rectangletest();
+
+        r1.setLength(7.1);
+        r1.setBreadth(7.1);
+        System.out.println("Area: "+r1.Area());
+        System.out.println("parameter: "+r1.parameter());
+        System.out.println("Square: "+r1.isSquare());
+
+        System.out.println(r1.getLength());
+        System.out.println(r1.getBreadth());
+    }
 }

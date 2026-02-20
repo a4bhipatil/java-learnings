7f92f1c396e83a7a5eb0c549d3d6f486c89a71d2
abhilash <abhilashpatil921@gmail.com>
2026-02-20 07:49:07 +0530

    It is also type of class and object concept problem and using method to print the lidarea,totalsurgacarea,circumference and volume

--- src/Cylinder1.java
+++ src/Cylinder1.java
@@ -7,17 +7,21 @@ class cylinder{
     double TotalSurfaceArea(){
         return 2*Math.PI*radias*(radias+height);
     }
+    double circumference(){
+        return 2*Math.PI*radias;
+    }
     double volume() {
         return Math.PI * radias * radias * height;
     }
 }
-public class Cylinder {
+public class Cylinder1 {
     public static void main(String[] args){
         cylinder c1=new cylinder();
         c1.radias=3;
         c1.height=9;
-        System.out.println(c1.Lidarea());
-        System.out.println(c1.TotalSurfaceArea());
-        System.out.println(c1.volume());
+        System.out.printf("Lid Area=%.2f\n ",c1.Lidarea());
+        System.out.printf("Total Surface Area of Cylinder=%.2f\n ",c1.TotalSurfaceArea());
+        System.out.printf("circumference=%.2f\n ",c1.circumference());
+        System.out.printf("Volume=%.2f\n ",c1.volume());
     }
 }

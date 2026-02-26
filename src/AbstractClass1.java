52a4ff244db406c13e4e643eca76784395e59869
abhilash <abhilashpatil921@gmail.com>
2026-02-26 07:13:48 +0530

    Abstract class on the hospital which has having methods like Myhospital,emergency,appointment,admit and billing

--- src/AbstractClass1.java
+++ src/AbstractClass1.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
+abstract class Hospital{
+    abstract void emergency();
+    abstract void appointment();
+    abstract void admit();
+    abstract void billing();
+}
+class Myhospital extends Hospital{
+    public Myhospital(){
+        System.out.println("Vasista hospital");
+    }
+    public void emergency(){
+        System.out.println("emergency");
+    }
+    public void appointment(){
+        System.out.println("appointment");
+    }
+    public void admit(){
+        System.out.println("admit");
+    }
+    public void billing(){
+        System.out.println("billing");
+    }
+}
 public class AbstractClass1 {
+    public static void main(String[] args){
+
+    }
 }

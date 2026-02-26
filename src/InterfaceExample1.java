82a9cc75d4d29d68edf2316ac89a4d08ad23935d
abhilash <abhilashpatil921@gmail.com>
2026-02-26 06:53:18 +0530

    It is the problem for interface

--- src/InterfaceExample1.java
+++ src/InterfaceExample1.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+interface Member{
+    void callback();
+}
+class Store{
+    Member men[]=new Member[100];
+    int count=0;
 
+    void register(Member m){
+        men[count++]=m;
+    }
+    void invitesale(){
+        for(int i=0; i<count; i++)
+            men[i].callback();
+    }
+}
+class Customer implements Member{
+    String name;
+    Customer(String n){
+        name=n;
+    }
+    public void callback(){
+        System.out.println("ok i will visit,"+name);
+    }
+}
 public class InterfaceExample1 {
+    public static void main(String[] args){
+        Store s=new Store();
+        Customer c1=new Customer("john");
+        Customer c2=new Customer("smith");
+        s.register(c1);
+        s.register(c2);
+        s.invitesale();
+    }
 }

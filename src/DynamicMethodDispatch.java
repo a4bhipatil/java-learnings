36fd6e47c77085a3059c19887d4d8b99b893a747
abhilash <abhilashpatil921@gmail.com>
2026-02-26 06:44:56 +0530

    A super class reference holding an object of su class and overrided method is dynamic dispatch

--- src/DynamicMethodDispatch.java
+++ src/DynamicMethodDispatch.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
+class Super1{
+    public void math1(){System.out.println("super math1");}
+    public void math2(){System.out.println("super math2");}
+}
+class Sub1 extends Super1{
+    public void math2(){System.out.println("sub math2");}
+    public void math3(){System.out.println("sub math3");}
+}
 public class DynamicMethodDispatch {
+    public static void main(String[] args){
+//        Super1 s=new Super1();
+//        s.math1();
+//        s.math2();
+
+//        Sub1 s=new Sub1();
+//        s.math1();
+//        s.math2();
+//        s.math3();
+
+        Super1 s=new Sub1();
+        s.math1();
+        s.math2();
+    }
 }

02acc251528fde5863dc07ccbc043817a75cf212
abhilash <abhilashpatil921@gmail.com>
2026-02-26 07:05:11 +0530

    Using static method to generate the roll number of student

--- src/StaticExaStudentRollNoGenerator.java
+++ src/StaticExaStudentRollNoGenerator.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
+import java.util.*;
+class Student{
+    private String rollno;
+    private static int count=1;
+    private String generator(){
+        Date d=new Date();
+        String m="Univ"+"-"+(d.getYear()+1900)+"-"+count;
+        count++;
+        return m;
+    }
+    public Student() {
+        rollno=generator();
+    }
+    public String getRollno(){
+        return rollno;
+    }
+}
 public class StaticExaStudentRollNoGenerator {
+    static public void main(String[] args){
+        Student s=new Student();
+        Student s1=new Student();
+        Student s2=new Student();
+
+        System.out.println(s.getRollno());
+        System.out.println(s1.getRollno());
+        System.out.println(s2.getRollno());
+
+    }
 }

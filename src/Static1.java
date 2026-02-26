71db18d0275b23c39bee4778df776d923c287cde
abhilash <abhilashpatil921@gmail.com>
2026-02-26 07:03:54 +0530

    all about static

--- src/Static1.java
+++ src/Static1.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
+class Test3{
+    static {
+        System.out.println("Block 1");
+    }
+    static {
+        System.out.println("Block 2");
+    }
+}
 public class Static1 {
+    public static void main(String[] args){
+        Test3 t=new Test3();
+        System.out.println("Main");
+    }
+
 }

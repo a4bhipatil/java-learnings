9a6af15e9be26f463025d693ca9f36470643d17e
abhilash <abhilashpatil921@gmail.com>
2026-02-26 06:57:40 +0530

    Nested classes means inner class will wrote inside the outer class

--- src/NestedInnerClass.java
+++ src/NestedInnerClass.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
+class Outer{
+    int x=10;
+    class Inner{
+        int y=20;
+        void innerdisplay(){
+            System.out.println(x+" "+y);
+        }
+    }
+    public void outerdisply(){
+        Inner i=new Inner();
+        i.innerdisplay();
+        System.out.println(i.y);
+    }
+}
 public class NestedInnerClass {
+    public static void main(String[] args){
+        Outer.Inner oi=new Outer().new Inner();
+        oi.innerdisplay();
+    }
 }

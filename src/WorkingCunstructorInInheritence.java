830b273c2eabe807eb9229e43bae2a1e25ad4ca5
abhilash <abhilashpatil921@gmail.com>
2026-02-26 07:10:38 +0530

    Using Inheritance and constructor to print the parent,child and grandchild constructor

--- src/WorkingCunstructorInInheritence.java
+++ src/WorkingCunstructorInInheritence.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
+class parent{
+    public parent(){
+        System.out.println("parent constructor");
+    }
+}
+class child extends parent{
+    public child(){
+        System.out.println("child constructor");
+    }
+}
+class grandchild extends child{
+    public grandchild(){
+        System.out.println("grandchild constructor");
+    }
+}
 public class WorkingCunstructorInInheritence {
+    public static void main(String[] args){
+        grandchild c=new grandchild();
+
+    }
 }

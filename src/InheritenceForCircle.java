3335272e677f8513551f2f16c1f16f2b58f1668c
abhilash <abhilashpatil921@gmail.com>
2026-02-26 06:47:34 +0530

    By using inheritance, cunstructor and get, set methods to access the private identifirs

--- src/InheritenceForCircle.java
+++ src/InheritenceForCircle.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
+class Circle2{
+    public double radias;
+    public double area(){
+        return Math.PI*radias*radias;
+    }
+    public double parimeter(){
+        return 2*Math.PI*radias;
+    }
+    public double circumference(){
+        return parimeter();
+    }
+}
+class Cylinder2 extends Circle2{
+    public double height;
+    public double volume(){
+        return area()*height;
+    }
+}
 public class InheritenceForCircle {
+    public static void main(String[] args){
+        Cylinder2 c=new Cylinder2();
+        c.radias=7;
+        c.height=10;
+        System.out.println(c.volume());
+        System.out.println(c.area());
+    }
 }

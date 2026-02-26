d35858e68774b46fd197a7d9ad4a5fa252317991
abhilash <abhilashpatil921@gmail.com>
2026-02-26 07:14:33 +0530

    Abstract class on the KFC

--- src/AbstractClass2.java
+++ src/AbstractClass2.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
+abstract class KFC{
+    KFC(){
+        System.out.println("rules of kfc");
+    }
+    public void makeitem(){
+        System.out.println("Making an item");
+    }
+    abstract void billing();
+    abstract void offer();
+}
+class Mykfc extends KFC{
+    Mykfc(){
+        System.out.println("Mykfc");
+    }
+    void billing(){
+        System.out.println("billing");
+    }
+    void offer(){
+        System.out.println("offers");
+    }
+    void festivaloffer(){
+        System.out.println("festival offer");
+    }
+}
 public class AbstractClass2 {
+    public static void main(String[] args){
+
+    }
 }

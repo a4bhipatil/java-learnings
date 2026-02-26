621840574d280a7eb4ae964e234e5171045f9c42
abhilash <abhilashpatil921@gmail.com>
2026-02-26 07:18:37 +0530

    Anonymous inner class will be having the interface or abstract for which to buld object inside the outer class

--- src/AnonymousInnerClass.java
+++ src/AnonymousInnerClass.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
+//Anonymous Inner class
+//abstract class My{
+interface My{
+    public void show();
+}
+class Outer2{
+    public void display(){
+        My m= new My() {
+            public void show() {
+                System.out.println("hello");
+            }
+        };
+        m.show();
+    }
+}
 public class AnonymousInnerClass {
+    public static void main(String[] args){
+        Outer1 o=new Outer1();
+        o.display();
+    }
 }

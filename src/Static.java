7eec2f9082017d187c7b6b9a555e91defc62456b
abhilash <abhilashpatil921@gmail.com>
2026-02-26 07:03:15 +0530

    it is all about static method which will only static will change

--- src/Static.java
+++ src/Static.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
+class Test2{
+    static int x=10;
+    int y=20;
+    void show(){
+        System.out.println(x+" "+y);
+    }
+    static void display(){
+        System.out.println(x);
+    }
+}
 public class Static {
+    public static void main(String[] args){
+        Test2 t1=new Test2();
+        t1.show();
+        t1.x=100;
+        t1.y=50;
+        Test2 t2=new Test2();
+        t2.show();
+        t2.display();
+    }
 }

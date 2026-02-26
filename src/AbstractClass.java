4fe8acc4904a0c333cd0032a69ac525f9848bf45
abhilash <abhilashpatil921@gmail.com>
2026-02-26 07:12:15 +0530

    The Abstract method which is having no body just semicolon written after the body it must be declarate abstract

--- src/AbstractClass.java
+++ src/AbstractClass.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
+abstract class Super2{
+    public Super2(){System.out.println("super constructor");}
+    public void math1(){
+        System.out.println("super math1");
+    }
+    abstract public void math2();
+}
+class Sub2 extends Super2{
+    public void math2(){
+        System.out.println("sub math2");
+    }
+}
 public class AbstractClass {
+    public static void main(String[] args){
+        Super2 s=new Sub2();
+        s.math1();
+        s.math1();
+    }
 }

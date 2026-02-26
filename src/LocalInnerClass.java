8a7854c6214b795fcd67873c316645e2c3a26893
abhilash <abhilashpatil921@gmail.com>
2026-02-26 06:54:18 +0530

    LocalInner class will be having innerclass inside the method

--- src/LocalInnerClass.java
+++ src/LocalInnerClass.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
-public class LocalInnerClass {
+class Outer1{
+    public void display(){
+        class Inner{
+            public void show(){
+                System.out.println("Hello");
+            }
+        }
+//        Inner i=new Inner();
+//        i.show();
+        new Inner().show();
+    }
 }
+public class LocalInnerClass {
+    public static void main(String[] args){
+        Outer1 o=new Outer1();
+        o.display();
+    }
+}
+
+
+

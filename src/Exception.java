cef83be041c08510e421c356ebff313720c27c7b
abhilash <abhilashpatil921@gmail.com>
2026-03-20 14:22:53 +0530

    It is all about exception handling and using try catch used to division problem

--- src/Exception.java
+++ src/Exception.java
@@ -1,12 +1,17 @@
-public class ExceptionDemo {
+import java.util.Scanner;
+
+public class Exception {
     public static void main(String[] args){
-        int a=10,b=0,c;
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c;
         try{
             c=a/b;
             System.out.println(c);
         }
         catch (ArithmeticException e){
-            System.out.println("Denominator should not be zero,try again");
+            System.out.println("Denominator should not be zero,try again "+e);
         }
         System.out.println("bye");
     }

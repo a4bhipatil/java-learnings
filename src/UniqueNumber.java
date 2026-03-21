4948fc6b0d920622aeaf2935b00b61bec935ab71
abhilash <abhilashpatil921@gmail.com>
2026-03-21 09:01:20 +0530

    This is the normal problem which is finding the unique number in the given array number

--- src/UniqueNumber.java
+++ src/UniqueNumber.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class UniqueNumber {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int uni=0;
+        for(int i=0; i<n; i++){
+            uni=uni^sc.nextInt();
+        }
+        System.out.println(uni);
+        sc.close();
+    }
 }

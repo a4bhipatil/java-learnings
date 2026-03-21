dea5579510d2a9f014d732c8b72e4fb3e8deb97c
abhilash <abhilashpatil921@gmail.com>
2026-03-21 08:56:55 +0530

    This is the just for practice perpose

--- src/Practice.java
+++ src/Practice.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
-public class Practice {
+import java.util.*;
+public class Practice{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=n;
+        int reverse=0;
+        while(n>0){
+            int r=n%10;
+            reverse=reverse*10+r;
+            n=n/10;
+        }
+        if(reverse==m)
+            System.out.println("it is a palindrome");
+        else
+            System.out.println("it is not a palindrome");
+    }
 }

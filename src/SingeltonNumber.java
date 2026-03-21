12e4f6798c4a4e943b03fbb036e513c66cfe1639
abhilash <abhilashpatil921@gmail.com>
2026-03-21 08:58:36 +0530

    This is the leet code problem which is finding the single number not repeated number

--- src/SingeltonNumber.java
+++ src/SingeltonNumber.java
@@ -1,2 +1,24 @@
+class Solution{
+    static int singlenumber(int[] nums){
+        int result=0;
+        for(int i=0; i<32; i++){
+            int sum=0;
+            for(int n:nums){
+                if(((n>>i)&1)==1){
+                    sum++;
+                }
+            }
+            if(sum%3!=0){
+                result|=(1<<i);
+            }
+        }
+        return result;
+    }
+}
 public class SingeltonNumber {
+    public static void main(String[] args){
+        int num1[]={2,2,3,2};
+        Solution n=new Solution();
+        System.out.println(n.singlenumber(num1));
+    }
 }

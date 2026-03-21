e3e41d7ee9e5db5c5be357213db890bc3079f1e1
abhilash <abhilashpatil921@gmail.com>
2026-03-21 08:51:56 +0530

    i already solved the palindrom of numbers and this is the palindrom of Strings

--- src/Palindrome_String.java
+++ src/Palindrome_String.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Palindrome_String {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String rev="";
+        for(int i=A.length()-1; i>=0; i--){
+            rev=rev+A.charAt(i);
+        }
+        if(A.equals(rev))
+            System.out.println("it is a palindrome");
+        else
+            System.out.println("it is not palindrome");
+    }
 }

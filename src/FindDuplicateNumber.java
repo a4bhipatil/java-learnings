88bdddb85006e6d99c48438c47ddef57176e5cea
abhilash <abhilashpatil921@gmail.com>
2026-03-21 08:48:14 +0530

    From the leet code the problem was solved

--- src/FindDuplicateNumber.java
+++ src/FindDuplicateNumber.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
+class Solution1{
+    public static List<Integer> Findduplicates(int[] nums){
+        List<Integer> result=new ArrayList<>();
+        for(int i=0; i<nums.length; i++){
+            int index=Math.abs(nums[i])-1;
+
+            if(nums[index]<0){
+                result.add(index+1);
+            }
+            else{
+                nums[index]=-nums[index];
+            }
+        }
+        return result;
+    }
+}
 public class FindDuplicateNumber {
+    public static void main(String[] args){
+        int nums[]={4,3,2,7,8,2,3,1};
+        List<Integer> duplicate= Solution1.Findduplicates(nums);
+        System.out.println(duplicate);
+    }
 }

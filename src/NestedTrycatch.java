3fcd65d2149dee8ec9ad73fc495d67303b325421
abhilash <abhilashpatil921@gmail.com>
2026-03-21 09:02:59 +0530

    Nested try catch is used and solved the problem of division of number

--- src/NestedTrycatch.java
+++ src/NestedTrycatch.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class NestedTrycatch {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int A[]=new int[5];
+        for(int i=0; i<A.length; i++){
+            A[i]=sc.nextInt();
+        }
+        try{
+            int r=A[0]/A[2];
+            System.out.println(r);
+            try{
+                System.out.println(A[5]);
+            }
+            catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Invalid index");
+            }
+        }
+        catch (ArithmeticException e){
+            System.out.println("Denominator should not be zero");
+        }
+        System.out.println("bye");
+    }
 }

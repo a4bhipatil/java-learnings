16526cc9f4dffa3612f2bc46ca9ca6037621e7dd
abhilash <abhilashpatil921@gmail.com>
2026-03-21 08:59:57 +0530

    This is the example for the try cath concept

--- src/TryCatchExample.java
+++ src/TryCatchExample.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.lang.Exception;
+import java.util.Scanner;
 
 public class TryCatchExample {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int A[]=new int[5];
+        for(int i=0; i<A.length; i++){
+            A[i]=sc.nextInt();
+        }
+        try{
+            int r=A[0]/A[2];
+            System.out.println(r);
+            System.out.println(A[5]);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Denominator should not be zero");
+        }
+        catch (Exception e){
+            System.out.println("Invalid index");
+        }
+        System.out.println("bye");
+    }
 }

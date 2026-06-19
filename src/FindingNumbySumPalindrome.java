6423ad0c5a8fc7a1635eb7acffa8dc58bf424913
abhilash <abhilashpatil921@gmail.com>
2026-06-19 11:41:19 +0530

    This problem is to find the sum of numbers is palindrome

--- src/FindingNumbySumPalindrome.java
+++ src/FindingNumbySumPalindrome.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class Palindrome {
+public class FindingNumbySumPalindrome {
+    static long reverse(long n){
+        long rev=0;
+        while(n>0){
+            long r=n%10;
+            rev=rev*10+r;
+            n=n/10;
+        }
+        return rev;
+    }
+    static boolean ispalindrome(long n){
+        return n==reverse(n);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextInt();
+        while(true) {
+            long rev = reverse(n);
+            //System.out.println(rev);
+            long sum = n + rev;
+            if (ispalindrome(sum)) {
+                System.out.println(sum);
+                break;
+            }
+            n=sum;
+        }
+        sc.close();
+    }
 }
